acbdd8e02ab6db49f17742451605b8778d34b01e
ostandmad <shashankpunna0@gmail.com>
2021-01-06 14:32:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/week6b/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.week6b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // initiate progress bar and start button
+        final ProgressBar simpleProgressBar = (ProgressBar) findViewById(R.id.simpleProgressBar);
+        Button startButton = (Button) findViewById(R.id.startButton);
+        // perform click event on button
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // visible the progress bar
+                simpleProgressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
